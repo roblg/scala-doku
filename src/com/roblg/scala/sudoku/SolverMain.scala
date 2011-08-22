f2e699976e8858cdc1ef434f2a8b835bbbe4d595
Robert Gay <robertgay@gmail.com>
2011-08-21 17:19:57 -0700

    Working. Prints Solution

--- src/com/roblg/scala/sudoku/SolverMain.scala
+++ src/com/roblg/scala/sudoku/SolverMain.scala
@@ -1,34 +1,48 @@
 package com.roblg.scala.sudoku
+
+import scala.collection.mutable.SetBuilder
+import scala.collection.mutable.Builder
+import scala.collection.mutable.HashSet
+import scala.collection.mutable.ListBuffer
  
 
-class Board(val initialData:Seq[Seq[Int]]) {
+case class BoardSect(r:Int, c:Int)
+
+class Board(val initialData:List[List[Int]]) {
   
   if (initialData.size != 9 || initialData(0).size != 9) {
     throw new RuntimeException
   }
   
-  // mutable boardData variable holding the current state
+  // immutable boardData variable holding the state of this board
   var boardData = initialData
   
   def isSolved = {
     cols.forall(c => c.forall(v => v > 0)) && 
-    		rows.forall(r => r.forall(v => v > 0))
-    // TODO include section here
+    		rows.forall(r => r.forall(v => v > 0)) &&
+    		sects.forall(s => s.forall(v => v > 0))
   }
   
-  private def unsolvedIndexes(colOrRow:Seq[Int]) = {
+  /*
+  def unsolvedIndexes(colOrRow:List[Int]) = {
     colOrRow.zip(0 to 8).		// pair up the values with their indexes
     	filter(p => p._1 > 0).	// remove the ones with the values == 0
     	map(p => p._2)			// just return the indexes
-  }
+  }*/
   
-  private def possibleVals(rowIdx:Int, colIdx:Int) = {
-    (unusedVals(getRow(rowIdx)) ++ unusedVals(getCol(colIdx)))
-  }
-  
-  private def unusedVals(colOrRow:Seq[Int]) = {
-    // TODO 
-    (0 to 8).toSet -- colOrRow
+  def possibleVals(rowIdx:Int, colIdx:Int) = {
+	  if (rows(rowIdx)(colIdx) != 0) {
+	    Set()
+	  } else {
+	    val b = new HashSet[Int]
+	    b ++= (1 to 9).toSet
+	    b --= getRow(rowIdx)
+	    b --= getCol(colIdx)
+	    b --= getSect(getSectForPos(rowIdx, colIdx))
+	    b.toSet
+	  }
+	  
+	  // (1 to 9).toSet -- getRow(rowIdx) -- getCol(colIdx) -- getSect(getSectForPos(rowIdx, colIdx))
   }
   
   /*
@@ -40,7 +54,7 @@ class Board(val initialData:Seq[Seq[Int]]) {
   }
   */
   
-  def setVal(r:Int, c:Int, v:Int) {
+  def withVal(r:Int, c:Int, v:Int) {
     boardData = boardData.updated(r, boardData(r).updated(c, v))
   }
   
@@ -49,8 +63,14 @@ class Board(val initialData:Seq[Seq[Int]]) {
   }
   
   def cols = {
-    (0 to 8).foldLeft(Seq[Seq[Int]]()) {
+    (0 to 8).foldLeft(List[List[Int]]()) {
       (acc, idx) => (acc :+ getCol(idx))
+    }
+  }
+  
+  def sects = {
+    (0 to 8).foldLeft(List[List[Int]]()) {
+      (acc, idx) => (acc :+ getSect(idx))
     }
   }
   
@@ -60,38 +80,80 @@ class Board(val initialData:Seq[Seq[Int]]) {
   
   def getCol(i:Int) = {
     // TODO this could be expensive?
-    boardData.foldLeft(Seq[Int]()) {
+    boardData.foldLeft(List[Int]()) {
       (acc, row) => (acc :+ row(i))
     }
   }
   
-  def getSectForPos(r:Int,j:Int) = {
-    
+  def getSectForPos(r:Int,c:Int) = {
+    // TODO: ugly 
+    r match {
+      case ridx if (ridx >= 0 && ridx <= 2) => c match {
+        case cidx if (cidx >= 0 && cidx <= 2) => 0
+        case cidx if (cidx >= 3 && cidx <= 5) => 1
+        case cidx if (cidx >= 6 && cidx <= 8) => 2
+      }
+      case ridx if (ridx >= 3 && ridx <= 5) => c match {
+        case cidx if (cidx >= 0 && cidx <= 2) => 3
+        case cidx if (cidx >= 3 && cidx <= 5) => 4
+        case cidx if (cidx >= 6 && cidx <= 8) => 5
+      }
+      case ridx if (ridx >= 6 && ridx <= 8) => c match {
+        case cidx if (cidx >= 0 && cidx <= 2) => 6
+        case cidx if (cidx >= 3 && cidx <= 5) => 7
+        case cidx if (cidx >= 6 && cidx <= 8) => 8
+      }
+    }
   }
   
   def getSect(i:Int) = {
+    val startRow = (i / 3) * 3
+    val startCol = (i % 3) * 3
     
+    val b = ListBuffer[Int]()
+    b ++= getRow(startRow).slice(startCol, startCol + 3) 
+    b ++= getRow(startRow+1).slice(startCol, startCol + 3)
+    b ++= getRow(startRow+2).slice(startCol, startCol + 3)
+    b.toList
   }
+  
+  override def toString = "[Board:" + rows.toString + "]"
   
 }
 
+class Pair(inX:Int, inY:Int) {
+  val x = inX
+  val y = inY  
+}
+
 object Board {
-  def apply(d:Seq[Seq[Int]]) = {
+  def apply(d:List[List[Int]]) = {
     new Board(d)
   }
   
+  def apply(d:Seq[Seq[Int]]) = {
+    new Board(d.foldLeft(List[List[Int]]()) {
+      (acc, r) => acc :+ r.toList
+    })
+  }
+  
   def solve(b:Board) {
+    if (b.isSolved) {
+      println(b)
+      // throw new RuntimeException
+    }
+    
     for (r <- 0 to 8) {
       for (c <- 0 to 8
           if b.rows(r)(c) == 0) {
-        for (v <- b.possibleVals(r, c)) {
-          b.setVal(r,c,v)
-          solve(b)
-          if (b.isSolved) {
-            return
+          val possibles = b.possibleVals(r,c)
+          // println(possibles)
+          for (v <- possibles) {
+            b.withVal(r, c, v)
+            solve(b)
+            b.withVal(r, c, 0)
           }
-          b.setVal(r,c,0)
-        }
+          return
       }
     }
   }
@@ -142,12 +204,15 @@ object SolverMain {
 //    println("Solved b2: " + b2.isSolved)
 //    
     // println(b.boardData)
-    // println(b.boardWithVal2(0,0,37))
+    //println(b.withVal(4,7,37))
     	
-    println(b.rows)
+    //println(b2.getSect(5))
+    
+//    println(b.rows)
     Board.solve(b)
-    println(b.isSolved)
-    println(b.rows)
+    //println(b.possibleVals(8,8))
+    //println(Board.solve(b).rows)
+//    
     // println(b.boardWithVal2(3,4,37))
     
     
